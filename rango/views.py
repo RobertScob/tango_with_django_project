12a82f771c098db9c63a57d78e2a78d4b6733848
“Robert <3018132s@student.gla.ac.uk>
2026-03-02 11:10:16 +0000

    6.2

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category, Page
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    categories = Category.objects.all()
-    context_dict = {'boldmessage': 'This tutorial has been put together by Robert', 'categories': categories}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Robert'}
     return render(request, 'rango/about.html', context=context_dict)
